b7d85388ba3706071b4330b83a18cf9ea9d5462c
Eric Xue <ovo4096@gmail.com>
2018-08-26 17:19:01 +0800

    feat: 添加开车 3 个按钮

--- src/views/kaiche.vue
+++ src/views/kaiche.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="kaiche">
-    <yd-button style="margin-top: 0" size="large" type="primary" shape="circle" @click.native="kaiche">替我开车</yd-button>
+    <yd-layout>
+      <yd-navbar slot="navbar" title="智能线上泊车" bgcolor="transparent" color="#fff" border-color="transparent">
+        <router-link to="#" slot="left">
+          <yd-navbar-back-icon></yd-navbar-back-icon>
+        </router-link>
+      </yd-navbar>
+      <div class="kaiche-content">
+        <img src="./car.png" alt="car" class="car">
+        <yd-button size="large" type="primary" shape="circle" @click.native="kaiche">替我开车</yd-button>
+      </div>
+      <yd-tabbar slot="tabbar">
+        <yd-tabbar-item title="招手" link="#" @click.native="hi">
+          <yd-icon name="good" slot="icon" size="0.54rem"></yd-icon>
+        </yd-tabbar-item>
+        <yd-tabbar-item title="左满转" link="#" @click.native="left">
+          <yd-icon name="prev" slot="icon" size="0.54rem"></yd-icon>
+        </yd-tabbar-item>
+        <yd-tabbar-item title="右满转" link="#" @click.native="right">
+          <yd-icon name="next" slot="icon" size="0.54rem"></yd-icon>
+        </yd-tabbar-item>
+      </yd-tabbar>
+    </yd-layout>
   </div>
 </template>
 
@@ -11,12 +32,21 @@
     left: 0;
     right: 0;
     bottom: 0;
-    padding: .5rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
     background: url("../assets/bak1.jpg");
     background-size: cover;
+  }
+
+  .kaiche-content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    padding: .5rem;
+  }
+
+  .car {
+    width: 100%;
   }
 </style>
 
@@ -24,10 +54,21 @@
   import { Loading, Toast } from 'vue-ydui/dist/lib.rem/dialog'
   import { Button } from 'vue-ydui/dist/lib.rem/button'
   import API from 'common/js/api'
+  import {TabBar, TabBarItem} from 'vue-ydui/dist/lib.rem/tabbar'
+  import {Icons} from 'vue-ydui/dist/lib.rem/icons'
+  import {Layout} from 'vue-ydui/dist/lib.rem/layout'
+  import {NavBar, NavBarBackIcon, NavBarNextIcon} from 'vue-ydui/dist/lib.rem/navbar';
 
   export default {
     components: {
-      'yd-button': Button
+      'yd-button': Button,
+      'yd-tabbar': TabBar,
+      'yd-tabbar-item': TabBarItem,
+      'yd-icon': Icons,
+      'yd-layout': Layout,
+      'yd-navbar': NavBar,
+      'yd-navbar-back-icon': NavBarBackIcon,
+      'yd-navbar-next-icon': NavBarNextIcon
     },
     methods: {
       kaiche () {
@@ -36,6 +77,75 @@
           deviceId: 'gh_db8607de9a99_41854abad30d591f',
           funcId: '010',
           value: '0101'
+        }
+        // 发送指令
+        Loading.open('发送中...')
+        this.$http.post(API.sendFunc, data).then(res => {
+          Loading.close()
+          if (res.code === 200) {
+            Toast({
+              mes: '发送成功',
+              timeout: 1500,
+              icon: 'success'
+            })
+          }
+        })
+        .catch(() => {
+          Loading.close()
+          console.log('发送失败')
+        })
+      },
+      hi () {
+        let data = {
+          deviceId: 'gh_db8607de9a99_41854abad30d591f',
+          funcId: '011',
+          value: '0'
+        }
+        // 发送指令
+        Loading.open('发送中...')
+        this.$http.post(API.sendFunc, data).then(res => {
+          Loading.close()
+          if (res.code === 200) {
+            Toast({
+              mes: '发送成功',
+              timeout: 1500,
+              icon: 'success'
+            })
+          }
+        })
+        .catch(() => {
+          Loading.close()
+          console.log('发送失败')
+        })
+      },
+      left () {
+        let data = {
+          deviceId: 'gh_db8607de9a99_41854abad30d591f',
+          funcId: '011',
+          value: '1'
+        }
+        // 发送指令
+        Loading.open('发送中...')
+        this.$http.post(API.sendFunc, data).then(res => {
+          Loading.close()
+          if (res.code === 200) {
+            Toast({
+              mes: '发送成功',
+              timeout: 1500,
+              icon: 'success'
+            })
+          }
+        })
+        .catch(() => {
+          Loading.close()
+          console.log('发送失败')
+        })
+      },
+      right () {
+        let data = {
+          deviceId: 'gh_db8607de9a99_41854abad30d591f',
+          funcId: '011',
+          value: '2'
         }
         // 发送指令
         Loading.open('发送中...')
