270b4557d6a676224432d868c82782aec246c300
shuiRong <shuirong1997@icloud.com>
2018-10-15 17:44:38 +0800

    feat: 🎸 替换分享给好友的微信接口
    BREAKING CHANGE: n
    
    Issues: n

--- src/utils/wx.js
+++ src/utils/wx.js
@@ -127,7 +127,7 @@ export function wxShare(title, desc, logo, link) {
   wx.ready(() => {
     // 所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，
     // 则可以直接调用，不需要放在ready函数中。
-    wx.updateAppMessageShareData({
+    wx.onMenuShareAppMessage({
       // 分享给朋友
       title: title, // 分享标题
       desc: desc, // 分享描述
