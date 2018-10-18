9ed1b8e465100bafb65cbcafa1553aa4b334b548
shuiRong <shuirong1997@icloud.com>
2018-10-18 21:50:46 +0800

    feat: 完善列表页删除设备的逻辑；两个新风版式增加根据天气更换背景图片的逻辑；完善模式指令发送的「值」可能错位的问题;

--- src/views/index/Index.vue
+++ src/views/index/Index.vue
@@ -199,7 +199,6 @@ export default {
       functionFlag: false,
       functionCurrent: null,
       functionData: [],
-      weather: null,
       currentSpeed: [],
       formatItemsList: [],
       abilitysList: [],
@@ -211,7 +210,8 @@ export default {
       deviceId: this.$route.query.deviceId,
       wxDeviceId: this.$route.query.wxDeviceId,
       customerId: this.$route.query.customerId,
-      setInter: undefined // 定时id
+      setInter: undefined, // 定时id
+      setInter2: undefined
     }
   },
   methods: {
@@ -528,7 +528,7 @@ export default {
       sendFunc({
         deviceId: this.deviceId,
         funcId: item.dirValue,
-        value: type === 3 ? index : item.abilityOptionList[index].dirValue
+        value: type === 3 ? index : item.abilityOptionList[index].optionValue
       })
         .then(res => {
           if (res.code === 200) {
@@ -549,7 +549,7 @@ export default {
               '指令发送成功:',
               item.dirValue,
               '-',
-              type === 3 ? index : item.abilityOptionList[index].dirValue
+              type === 3 ? index : item.abilityOptionList[index].optionValue
             )
           }
         })
@@ -587,6 +587,10 @@ export default {
           this.setInter = setInterval(() => {
             this.getIndexFormatData()
           }, 1000)
+
+          this.setInter2 = setInterval(() => {
+            this.getWeather()
+          }, 60000)
         }
       })
     },
@@ -640,6 +644,53 @@ export default {
         }
       })
     },
+    setWether() {
+      var myDate = new Date()
+      let h = myDate.getHours() //获取当前小时
+      if (!this.weather) {
+        //未返回值
+        if (
+          (parseInt(h) < 6 && parseInt(h) >= 0) ||
+          (parseInt(h) < 24 && parseInt(h) > 18)
+        ) {
+          //夜晚
+          this.currentBak = img3
+        } else {
+          //白天
+          this.currentBak = img4
+        }
+      } else {
+        if (
+          this.weather.indexOf('雨') != -1 ||
+          this.weather.indexOf('阴') != -1
+        ) {
+          //阴天
+          if (
+            (parseInt(h) < 6 && parseInt(h) >= 0) ||
+            (parseInt(h) < 24 && parseInt(h) > 18)
+          ) {
+            //夜晚
+            this.currentBak = img2
+          } else {
+            //白天
+            this.currentBak = img1
+          }
+        } else {
+          if (
+            (parseInt(h) < 6 && parseInt(h) >= 0) ||
+            (parseInt(h) < 24 && parseInt(h) > 18)
+          ) {
+            //夜晚
+            this.currentBak = img3
+          } else {
+            //白天
+            this.currentBak = img4
+          }
+        }
+      }
+      console.log('this,', this.currentBak)
+      this.img = this.currentBak
+    },
     getLocation() {
       getLocation(this.deviceId).then(res => {
         const data = res.data
@@ -668,6 +719,8 @@ export default {
         this.outerTem = data.outerTem
         this.outerPm = data.outerPm
         this.outerHum = data.outerHum
+
+        this.setWether()
       })
     },
     switchHandler() {
@@ -730,6 +783,7 @@ export default {
   },
   destroyed() {
     clearInterval(this.setInter)
+    clearInterval(this.setInter2)
   }
 }
 </script>
